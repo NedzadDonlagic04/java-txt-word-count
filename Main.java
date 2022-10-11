5e619b4af4bd397ae343574a96aa60ec96342530
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-11 14:49:39 +0200

    Added example.txt creation condition
    In case example.txt is deleted by the user and
    they try to run the program, first it will be
    created and then will functional normally upon
    second start.

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args)
@@ -11,7 +12,28 @@ public class Main {
         }
         else
         {
-            System.out.println("File doesn't exists!");
+            printErrorMsg(file);
+
+            createFile(file);
+        }
+    }
+
+    private static void printErrorMsg(File file)
+    {
+        System.out.println("File doesn't exists!");
+        System.out.print("If you wish to use this program you need to input the text inside the created ");
+        System.out.println("\"" + file.getName() + "\" file!");
+    }
+
+    private static void createFile(File newFile)
+    {
+        try {
+            newFile.createNewFile();
+        }
+        catch(IOException error)
+        {
+            System.out.println("Error ocurred during file creation!");
+            error.printStackTrace();
         }
     }
 }
