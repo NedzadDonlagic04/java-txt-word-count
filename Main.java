a912f06688c0d58cba00e43f21fb42672d898e11
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-11 14:39:05 +0200

    Added checking does passed file exist

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        File file = new File("./example.txt");
+
+        if(file.exists())
+        {
+            System.out.println("File exists!");
+        }
+        else
+        {
+            System.out.println("File doesn't exists!");
+        }
     }
 }
