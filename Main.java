b4983a7d64e65d3405cd4c19e55b501fa0aa66d7
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-11 17:03:50 +0200

    Added hashmap to save the word counts
    The words are the key and the count of the words
    is the value. Using this I made a few methods
    to make it look more clean.

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args)
@@ -10,30 +11,54 @@ public class Main {
 
         if(file.exists())
         {   
-            try {
-                Scanner scanner = new Scanner(file);
-
-                while(scanner.hasNextLine())
-                {
-                    String[] line = scanner.nextLine().split(" ");
-                    for(int i=0; i<line.length; i++)
-                    {
-                        System.out.println(line[i]);
-                    }
-                }
-
-                scanner.close();
-            }
-            catch(FileNotFoundException error)
-            {
-                error.printStackTrace();
-            }
+            HashMap<String, Integer> words = new HashMap<>();
+            countTheWords(file, words);
+            outputHashMap(words);
         }
         else
         {
             printErrorMsg(file);
 
             createFile(file);
+        }
+    }
+
+    private static void outputHashMap(HashMap<String, Integer> map)
+    {
+        System.out.println("Key => Value");
+        map.entrySet().forEach(entry -> {
+            System.out.println( entry.getKey() + " => " + entry.getValue() );
+        });
+    }
+
+    private static void countTheWords(File file, HashMap<String, Integer> map)
+    {
+        try {
+            Scanner scanner = new Scanner(file);
+
+            while(scanner.hasNextLine())
+            {
+                String[] line = scanner.nextLine().split(" ");
+                for(int i=0; i<line.length; i++)
+                {
+                    String gottenLine = line[i].toLowerCase();
+                    if(map.containsKey(gottenLine))
+                    {
+                        int value = map.get(gottenLine) + 1;
+                        map.replace(gottenLine, value);
+                    }
+                    else
+                    {
+                        map.put(gottenLine, 1);
+                    }
+                }
+            }
+
+            scanner.close();
+        }
+        catch(FileNotFoundException error)
+        {
+            error.printStackTrace();
         }
     }
 
