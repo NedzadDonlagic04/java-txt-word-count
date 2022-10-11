6fb72c168679fbbc9a71a91e7cf8ea0549eac2da
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-11 15:43:33 +0200

    Added reading file line by line
    As well as outputting those lines after they've
    been split based on spaces.

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args)
@@ -7,8 +9,25 @@ public class Main {
         File file = new File("./example.txt");
 
         if(file.exists())
-        {
-            System.out.println("File exists!");
+        {   
+            try {
+                Scanner scanner = new Scanner(file);
+
+                while(scanner.hasNextLine())
+                {
+                    String[] line = scanner.nextLine().split(" ");
+                    for(int i=0; i<line.length; i++)
+                    {
+                        System.out.println(line[i]);
+                    }
+                }
+
+                scanner.close();
+            }
+            catch(FileNotFoundException error)
+            {
+                error.printStackTrace();
+            }
         }
         else
         {
